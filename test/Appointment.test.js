36b509349a8751a0a91264424111496490511ebb
Steve Gelman <sgelman@nextpoorslob.com>
2022-11-13 17:49:59 -0600

    First test passed.

---
+++ test/Appointment.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {act} from "react-dom/test-utils";
+import {Appointment} from "../src/Appointment";
+
+describe("Appointment", () => {
+    it("renders the customer first name", () => {
+        const customer = {firstName: "Ashley"};
+        const component = (
+            <Appointment customer={customer}/>
+        );
+        const container = document.createElement("div");
+        document.body.replaceChildren(container);
+        act(() =>
+            ReactDOM.createRoot(container).render(component)
+        );
+        expect(document.body.textContent).toContain(
+            "Ashley"
+        );
+    });
+
+    it("renders another customer first name", () => {
+        const customer = {firstName: "Jordan"};
+        const component = (
+            <Appointment customer={customer}/>
+        );
+        const container = document.createElement("div");
+        document.body.replaceChildren(container);
+        act(() =>
+            ReactDOM.createRoot(container).render(component)
+        );
+        expect(document.body.textContent).toContain(
+            "Jordan"
+        );
+    });
+});
